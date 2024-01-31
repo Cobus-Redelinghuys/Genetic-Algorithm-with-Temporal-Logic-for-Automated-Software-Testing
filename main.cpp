ab210cfaba6d1582f3233035d16bef5a4558e09e
Cobus Redelinghuys <72762130+DarkMerlin1@users.noreply.github.com>
2024-01-31 14:50:32 +0200

    Completed the triangle program

--- main.cpp
+++ main.cpp
@@ -6,8 +6,31 @@
 
 using namespace std;
 
-string triangleType(int s1, int s2, int s3){
-    
+#define equilateral "equilateral"
+#define isosceles "isosceles"
+#define scalene "scalene"
+#define not_a_triangle "not a triangle"
+
+string triangleType(int a, int b, int c)
+{
+    string type;
+    if (a + b <= c)
+    {
+        type = not_a_triangle;
+    }
+    else
+    {
+        type = scalene;
+        if (a == b && b == c)
+        {
+            type = equilateral;
+        }
+        else if (a == b || b == c)
+        {
+            type = isosceles;
+        }
+    }
+    return type;
 }
 
 int main()
@@ -24,7 +47,6 @@ int main()
     sides.push_back(stoi(strV2));
     sides.push_back(stoi(strV3));
     sort(sides.begin(), sides.end());
-    for (auto x : sides) 
-        cout << x << " "; 
     file.close();
+    cout << triangleType(sides[0], sides[1], sides[2]) << endl;
 }
--- makefile
+++ makefile
@@ -1,4 +1,13 @@
-main:
-	rm main -f
-	g++ -g *.cpp -o main
+run:
 	./main
+
+clean:
+	rm main -f
+
+compile:
+	g++ -g *.cpp -o main
+
+all:
+	make clean -s
+	make compile -s
+	make run -s
